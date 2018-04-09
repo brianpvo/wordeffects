23d4e32666bfbcc9e2101eda1d1daebf5e2e5db8
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-04-09 13:53:43 -0700

    Part 1

---
+++ WordEffects/WordEffects/main.m
@@ -0,0 +1,32 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Brian Vo on 2018-04-09.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        while (1) {
+            // 255 unit long array of characters
+            char inputChars[255];
+            
+            printf("Input a string: ");
+            // limit input to max 255 characters
+            fgets(inputChars, 255, stdin);
+            
+            // print as a c string
+            printf("Your string is %s\n", inputChars);
+            
+            // convert char array to an NSString object
+            NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            
+            // print NSString object
+            NSLog(@"Input was: %@", inputString);
+        }
+    }
+    return 0;
+}
